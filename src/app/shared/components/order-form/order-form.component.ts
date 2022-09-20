9e28ef734d2c6addff2da1d083784fc4a243f109
MarkDat <mark.dat.code@gmail.com>
2022-09-20 17:18:10 +0700

    Format code

--- src/app/shared/components/order-form/order-form.component.ts
+++ src/app/shared/components/order-form/order-form.component.ts
@@ -3,13 +3,13 @@ import { Dish, DishOrder } from '@app/models';
 import * as lodash from 'lodash';
 
 @Component({
-  selector: 'app-order-form',
-  templateUrl: './order-form.component.html',
-  styleUrls: ['./order-form.component.scss']
+	selector: 'app-order-form',
+	templateUrl: './order-form.component.html',
+	styleUrls: ['./order-form.component.scss']
 })
 export class OrderFormComponent implements OnInit {
 
-  private _ordersSource: DishOrder[];
+	private _ordersSource: DishOrder[];
 	@Input() get ordersSource(): DishOrder[] {
 		return this._ordersSource || [];
 	}
@@ -17,48 +17,48 @@ export class OrderFormComponent implements OnInit {
 	set ordersSource(value) {
 		if (this._ordersSource !== value) {
 			this._ordersSource = value;
-      this.ordersSourceChange.emit(value);
+			this.ordersSourceChange.emit(value);
 		}
 	}
 
-  get orderEmpty(): boolean {
-    return this.ordersSource.some(_ => _.id);
-  }
+	get orderEmpty(): boolean {
+		return this.ordersSource.some(_ => _.id);
+	}
 
-  get total(): number {
-    if(!this.ordersSource) {
-      return 0;
-    }
+	get total(): number {
+		if (!this.ordersSource) {
+			return 0;
+		}
 
-    return lodash.sumBy(this.ordersSource, function(o) { return o.price.value * o.quantity; });
-  }
+		return lodash.sumBy(this.ordersSource, function (o) { return o.price.value * o.quantity; });
+	}
 
-  @Output() ordersSourceChange = new EventEmitter<DishOrder[]>();
+	@Output() ordersSourceChange = new EventEmitter<DishOrder[]>();
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
-  addQuantity(dishOrder: DishOrder) {
-    dishOrder.quantity++;
-    
-  }
+	addQuantity(dishOrder: DishOrder) {
+		dishOrder.quantity++;
 
-  minusQuantity(dishOrder: DishOrder) {
-    dishOrder.quantity--;
+	}
 
-    if(dishOrder.quantity <= 0) {
-      this.removeOrder(dishOrder.id);
-    }
-  }
+	minusQuantity(dishOrder: DishOrder) {
+		dishOrder.quantity--;
 
-  removeOrder(id: number) {
-    const dishOrderIndex = this.ordersSource.findIndex(_ => _.id === id);
-    this.ordersSource.splice(dishOrderIndex, 1);
-  }
+		if (dishOrder.quantity <= 0) {
+			this.removeOrder(dishOrder.id);
+		}
+	}
 
-  clearCart(e) {
-    this.ordersSource = [];
-  }
+	removeOrder(id: number) {
+		const dishOrderIndex = this.ordersSource.findIndex(_ => _.id === id);
+		this.ordersSource.splice(dishOrderIndex, 1);
+	}
+
+	clearCart(e) {
+		this.ordersSource = [];
+	}
 }
